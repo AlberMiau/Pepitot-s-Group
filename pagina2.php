706ce328e6fb7cc6f758efa5893633edb336fbf5
AlberMiau <Ascendia.Alberto@gmail.com>
2018-05-02 17:33:11 +0200

    Alber1
    Pag2 sin CSS

---
+++ pagina2.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<?php include'includes/cabecera.php' ?>
+
+<div>
+	<h1>Windows Frequently Asked Questions</h1>
+</div>
+
+<div class="container">
+	
+<h3>How do I install XAMPP?</h3><hr>
+<h3>Does XAMPP include MySQL or MariaDB?</h3><hr>
+<h3>How can I start XAMPP without setup?</h3><hr>
+<h3>How do I start and stop XAMPP?</h3><hr>
+<h3>How can I test that everything worked?</h3><hr>
+<h3>How can I install a server as a service?</h3><hr>
+<h3>Is XAMPP production ready?</h3><hr>
+<h3>How do I uninstall XAMPP?</h3><hr>
+<h3>What is the "lite" version of XAMPP?</h3><hr>
+<h3>Where should I place my web content?</h3><hr>
+<h3>Can I move the XAMPP installation?</h3><hr>
+<h3>What are "Automatic start pages" for the WWW directories?</h3><hr>
+<h3>Where can I change the configuration?</h3><hr>
+<h3>Why can't XAMPP work on Windows XP SP2?</h3><hr>
+<h3>Why doesn't XAMPP work on Vista?</h3><hr>
+<h3>How do I check the md5 checksum?</h3><hr>
+<h3>Why have changes in my php.ini not taken effect?</h3><hr>
+<h3>Help! There is a virus in XAMPP!</h3><hr>
+<h3>How do I configure my antivirus application?</h3><hr>
+<h3>Why doesn't the Apache server start on my system?</h3><hr>
+<h3>Why is my CPU load for Apache almost 99%?</h3><hr>
+<h3>Why are pictures and style-sheets not displayed?</h3><hr>
+<h3>How can I set a root password in MySQL?</h3><hr>
+<h3>Can I use my own MySQL server?</h3><hr>
+<h3>How do I restrict access to phpMyAdmin from the outside?</h3><hr>
+<h3>How do I enable access to phpMyAdmin from the outside?</h3><hr>
+<h3>Where is the IMAP support for PHP?</h3><hr>
+<h3>Why don't some PHP open source apps work with XAMPP on Windows?</h3><hr>
+<h3>Can I delete the "install" directory after installation?</h3><hr>
+<h3>How do I activate the eaccelerator?</h3><hr>
+<h3>How do I fix a connection error to my MS SQL server?</h3><hr>
+<h3>How do I work with the PHP mcrypt extension?</h3><hr>
+<h3>Do Microsoft Active Server Pages (ASP) work with XAMPP?</h3><hr>
+<h3>How can I get XAMPP working on port 80 under Windows 10?</h3><hr>
+<h3>How can I use Microsoft Edge to access local addresses under Windows 10?</h3><hr>
+</div>
+
+
+<?php include 'includes/pie.php' ?>
+</body>
+</html>
