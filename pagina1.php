97854bc2dca81de6e083496359097b9bbe094918
AlberMiau <Ascendia.Alberto@gmail.com>
2018-05-02 20:37:43 +0200

    Albert
    Otro1

--- pagina1.php
+++ pagina1.php
@@ -5,14 +5,15 @@
 </head>
 <body>
 <?php include 'inc/header.php' ?>
-<div class="container coloretear">
+<div class="coloretear">
+<div class="container">
 	<p>Apache Friends and Bitnami are cooperating to make dozens of open source applications available on XAMPP, for free. Bitnami-packaged applications include Wordpress, Drupal, Joomla! and dozens of others and can be deployed with one-click installers. Visit the <a href="#">Bitnami XAMPP page</a> for details on the currently available apps.</p>
 
 
 <p>Check out our <a href="#">Bitnami for XAMPP Start Guide</a> for more information about the applications installed.</p>
 <div class="mismolol">We are working on making Bitnami add-ons available for PHP 7, it is possible the current add-ons do not work well with this version.</div>
 </div>
-
+</div>
 <?php include 'inc/footer.php' ?>
 </body>
 </html>
