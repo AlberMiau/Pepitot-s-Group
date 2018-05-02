dd82162c9446652aed45227031a2a9835cfd5b5c
Javi en el Curso <ascendia.javier@gmail.com>
2018-05-02 20:38:51 +0200

    Adaptacion para navegacion el el site

--- pagina4.php
+++ pagina4.php
@@ -1,3 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include'inc/head.php' ?>
+</head>
+<body>
+<?php 
+    include'inc/header.php' 
+    
     phpinfo ();
+    
+    include'inc/footer.php';
 ?>
+</body>
+</html>
