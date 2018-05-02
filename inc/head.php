861b0db2e1e4ee04cd1b32fd6a6af8cb694d361b
Javi en el Curso <ascendia.javier@gmail.com>
2018-05-02 17:40:07 +0200

    archivos includes

---
+++ inc/head.php
@@ -0,0 +1,25 @@
+
+<!-- Basic Page Needs
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<meta charset="utf-8">
+<title>Your page title here :)</title>
+<meta name="description" content="">
+<link rel="author" type="text/plain" href="humans.txt" />
+
+<!-- Mobile Specific Metas
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- FONT
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<!-- link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css" -->
+
+<!-- CSS
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
+
+<!-- Favicon
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<link rel="icon" type="image/png" href="img/favicon.png">
---
+++ inc/jscript.php
@@ -0,0 +1,11 @@
+<script type="text/JavaScript" src="js/jquery-3.3.1.min.js" >
+// Script personal.
+</script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous">
+  // Script Bootstrap 3
+</script>
+
+<script type="text/JavaScript" src="js/jscript.js" >
+// Script personal.
+</script>
