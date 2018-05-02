77c8ef2c95161071fd50a755314bb7370433fb54
alumno_mlalva <acendia.manuel@gmail.com>
2018-05-02 19:56:46 +0200

    añadido footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,27 @@
 <footer>
- 
-</footer>
+		<div class="parte1">
+			<ul>
+				<li><a href="#">twitter</a></li>
+				<li><a href="#">facebook</a></li>
+				<li><a href="#">google+</a></li>
+			</ul>
+
+		</div>	
+		
+		<div class="parte2">
+			<ul>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">Privacy Policy</a></li>
+				<li><a href="#">CDN Provided by <strong>fastly</strong></a></li>
+				
+			</ul>
+		</div>
+		
+		<div class="parte3">
+			<p>Copyright (c) 2017, Apache Friends</p>
+		</div>
+		
+		
+
+
+	</footer>
