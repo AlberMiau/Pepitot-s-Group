a2119c3b739b6f303a52a96475f84310b5b71c88
Chary_Jimenez <ascendia.chary@gmail.com>
2018-05-02 19:02:07 +0200

    añadida pagina3

---
+++ pagina3.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>XAMPPP howto guides</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/skeleton.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+   <div id="cuerpo_pagina3">
+      	<div class="container_page3">
+		  <div class="row">
+		    <div class="large-12 columns">
+		      <h1>Windows 
+		      	<span>HOW-TO Guides</span>
+		      </h1>
+		    </div>
+		  </div>
+		</div>
+
+		<div class="row">
+		  <div class="large-12 columns listado">
+					<h5><a href="#">Backup and Restore MySQL/MariaDB Databases</a></h5>
+					<h5><a href="#">Configure FTP Access</a></h5>
+					<h5><a href="#">Configure Virtual Hosts</a></h5>
+					<h5><a href="#">Configure Wildcard-Based Subdomains</a></h5>
+					<h5><a href="#">Deploy an Application Using Git</a></h5>
+					<h5><a href="#">Get Started Quickly with WordPress</a></h5>
+					<h5><a href="#">Reset the MySQL/MariaDB Root Password</a></h5>
+					<h5><a href="#">Send Mail with PHP</a></h5>
+					<h5><a href="#">Use SQLite with PHP</a></h5>
+					<h5><a href="#">Activate and Use Xdebug</a></h5>
+					<h5><a href="#">Start a New Zend Framework 1.x Project</a></h5>
+					<h5><a href="#">Start a New Zend Framework 2.x Project</a></h5>
+					<h5><a href="#">Troubleshoot Apache Startup Problems</a></h5>
+					<h5><a href="#">Change the MySQL Temporary Files Directory</a></h5>
+					<h5><a href="#">Increase the PHP File Upload Limit</a></h5>
+					<h5><a href="#">Enable Remote Access to phpMyAdmin</a></h5>
+					<h5><a href="#">Auto-Start XAMPP</a></h5>
+		    		<h5><a href="#">Use a Different PHP Version</a></h5>
+					<h5><a href="#">Run PHP under FastCGI</a></h5>
+					<h5><a href="#">Configure and Use Apache Tomcat</a></h5>
+
+		  </div>
+		</div>
+
+    </div>
+
+
+</body>
+</html>
