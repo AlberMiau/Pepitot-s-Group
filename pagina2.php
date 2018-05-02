cecb61dab581dc300ea83b4cf94aae22186aa623
AlberMiau <Ascendia.Alberto@gmail.com>
2018-05-02 19:56:12 +0200

    Alber
    phpHead

--- pagina2.php
+++ pagina2.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/skeleton.css">
-	<link rel="stylesheet" href="normalize.css">
+	<?php include'inc/head.php' ?>
 </head>
 <body>
 <?php include'inc/header.php' ?>
