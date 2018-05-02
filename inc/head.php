660fb15d3fc3cc54984b5ab3d100e6aa2dc48a38
Javi en el Curso <ascendia.javier@gmail.com>
2018-05-02 19:20:16 +0200

    Añadida la linea de fontawesome

--- inc/head.php
+++ inc/head.php
@@ -2,8 +2,8 @@
 <!-- Basic Page Needs
 –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 <meta charset="utf-8">
-<title>Your page title here :)</title>
-<meta name="description" content="">
+<title>Welcome to XAMPP</title>
+<meta name="description" content="Página desarrollada en equipo en el curso de diseño web">
 <link rel="author" type="text/plain" href="humans.txt" />
 
 <!-- Mobile Specific Metas
@@ -13,11 +13,11 @@
 <!-- FONT
 –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 <!-- link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css" -->
+<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
 
 <!-- CSS
 –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 <link rel="stylesheet" href="css/normalize.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 <link rel="stylesheet" href="css/style.css">
 
 <!-- Favicon
