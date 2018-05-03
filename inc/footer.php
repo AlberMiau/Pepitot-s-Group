36f1fac2646ef6ee6b1ea86f922ec07cccff9c7e
AlberMiau <Ascendia.Alberto@gmail.com>
2018-05-03 18:59:11 +0200

    Alberto
    FooterCONCSS

--- inc/footer.php
+++ inc/footer.php
@@ -1,27 +1,25 @@
 <footer>
-		<div class="parte1">
-			<ul>
-				<li><a href="#">twitter</a></li>
-				<li><a href="#">facebook</a></li>
-				<li><a href="#">google+</a></li>
-			</ul>
-
-		</div>	
-		
-		<div class="parte2">
-			<ul>
-				<li><a href="#">Blog</a></li>
-				<li><a href="#">Privacy Policy</a></li>
-				<li><a href="#">CDN Provided by <span class="blanco">fastly</span></a></li>
-				
-			</ul>
+	<div class="container">
+		<div class="row">
+			<div class="social">
+				<ul>
+					<li><a href="#twitter"><div class="twit"></div></a></li>
+					<li><a href="#facebook"><div class="faceb"></div></a></li>
+					<li><a href="#Google"><div class="googl"></div></a></li>
+				</ul>
+			</div>
+			<div class="lista">
+				<ul>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Privacy Policy</a></li>
+					<li><a href="#">CDN Provided by <img width="48" src="img/fastly.png" alt="fastly"></a></li>
+					
+				</ul>
+			</div>
+			
+			<div class="parte3">
+				<p>Copyright (c) 2017, Apache Friends</p>
+			</div>
 		</div>
-		
-		<div class="parte3">
-			<p>Copyright (c) 2017, Apache Friends</p>
-		</div>
-		
-		
-
-
-	</footer>
+	</div>
+</footer>
