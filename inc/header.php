a4cc3a5162ef69747da4d0b554717a1d402840f4
AlberMiau <Ascendia.Alberto@gmail.com>
2018-05-03 18:26:01 +0200

    Revert "cambios en la barra de header"
    This reverts commit 7c12ec52b8f736fa146f692f9c01333b69bc3741.

--- inc/header.php
+++ inc/header.php
@@ -1,16 +1,13 @@
-<nav>
-    <div class="container">
-        <div class="logo">
-            <a href="index.php">Apache Friends</a>
-        </div>  
-     <div class="menu">
-            <ul>
-             <li><a href="pagina1.php">Aplications</a></li>
-             <li><a href="pagina2.php">FAQs</a></li>
-             <li><a href="pagina3.php">HOW-TO Guides</a></li>
-             <li><a href="pagina4.php">PHPInfo</a></li>
-          <li><a href="http://localhost/phpmyadmin/">phpMyAdmin</a></li>
-         </ul>
-     </div>
-    </div>
+<nav class="button">
+    
+        <ul class="apache">
+            <li><a href="index.php">Apache Friends</a></li>
+        </ul>  
+    <ul class="apache2">
+        <li><a href="pagina1.php">Aplications</a></li>
+        <li><a href="pagina2.php">FAQs</a></li>
+        <li><a href="pagina3.php">HOW-TO Guides</a></li>
+        <li><a href="pagina4.php">PHPInfo</a></li>
+        <li><a href="http://localhost/phpmyadmin/">phpMyAdmin</a></li>
+    </ul>
 </nav>
