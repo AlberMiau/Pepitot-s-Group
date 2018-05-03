56429045488156c2fe0f0839ae54e185046fd552
Javier Gonzalez <javifinal3@gmail.com>
2018-05-03 17:13:27 +0200

    enlaces corregido index

--- index.php
+++ index.php
@@ -27,12 +27,12 @@
 </div>
 <div class="row">
   <div class="twelve columns">
-    <p>
+    <p class="enlace_index">
       You have successfully installed XAMPP on this system! Now you can start using Apache, MariaDB, PHP and other components.
       You can find more info in the <a href="pagina3.php">HOW-TO Guides</a> for getting started with PHP applications.
       
     </p>
-    <p>
+    <p class="enlace_index">
       XAMPP is meant only for development purposes. It has certain configuration settings that make it easy to develop locally but that are insecure if you want to have your installation accessible to others.
       If you want have your XAMPP accessible from the internet, make sure you understand the implications and you checked the <a href="pagina2.php">FAQs</a> to learn how to protect your site. Alternatively you can use <a href="https://bitnami.com/stack/wamp">WAMP</a>, <a href="https://bitnami.com/stack/mamp">MAMP</a> or <a href="https://bitnami.com/stack/lamp">LAMP</a> which are similar packages which are more suitable for production.
     </p>
@@ -48,19 +48,19 @@
 </div>
 <div class="row">
   <div class="twelve columns">
-    <p>
+    <p class="enlace_index">
       XAMPP has been around for more than 10 years – there is a huge community behind it. You can get involved by joining our <a href="https://community.apachefriends.org">Forums</a>, adding yourself to the <a href="https://www.apachefriends.org/community.html#mailing_list">Mailing List</a>, and liking us on <a href="https://www.facebook.com/we.are.xampp">Facebook</a>, following our exploits on <a href="https://twitter.com/apachefriends">Twitter</a>, or adding us to your <a href="https://plus.google.com/+xampp/posts">Google+</a> circles.
     </p>
   </div>
 </div>
 <div class="row">
   <div class="twelve columns">
-    <h3>Contribute to XAMPP translation at <a href="https://translate.apachefriends.org/">translate.apachefriends.org</a>.</h3>
+    <h3 class="enlace_index">Contribute to XAMPP translation at <a href="https://translate.apachefriends.org/">translate.apachefriends.org</a>.</h3>
   </div>
 </div>
 <div class="row">
   <div class="twelve columns">
-    <p>
+    <p class="enlace_index">
       Can you help translate XAMPP for other community members? We need your help to translate XAMPP into different languages. We have set up a site, <a href="https://translate.apachefriends.org/">translate.apachefriends.org</a>, where users can contribute translations.
     </p>
   </div>
@@ -72,7 +72,7 @@
 </div>
 <div class="row">
   <div class="twelve columns">
-    <p>
+    <p class="enlace_index">
     Apache Friends and Bitnami are cooperating to make dozens of open source applications available on XAMPP, for free. Bitnami-packaged applications include Wordpress, Drupal, Joomla! and dozens of others and can be deployed with one-click installers.
     Visit the <a target="_blank" href="http://bitnami.com/stack/xampp?utm_source=bitnami&amp;utm_medium=installer&amp;utm_campaign=XAMPP%2BModule">Bitnami XAMPP page</a> for details on the currently available apps.
     </p>
